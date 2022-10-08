511956d7b5aec12ac4b7ceb2933d368f227356b6
MegaRaman <roma.rio18121811@gmail.com>
2022-10-08 19:10:32 +0200

    Lab1: Add makefile and restructurize project

--- Lab_1/IO-01_Spivachuk_Roman/lab1sp.c
+++ Lab_1/IO-01_Spivachuk_Roman/lab1sp.c
@@ -1,65 +1,86 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "lab1sp.h"
 
+struct header_struct {
+    char chunk_id[4];
+    uint32_t chunk_size;
+    char format[4];
 
-int main(int argc, char **argv) {
-    if (argc != 4) {
-        printf("Wrong parameter count. Usage: ./%s File_from_read File_to_write Multiply_value\n", *argv);
+    char subchunk1_id[4];
+    uint32_t subchunk1_size;
+    uint16_t audio_format;
+    uint16_t num_channels;
+    uint32_t sample_rate;
+    uint32_t byte_rate;
+    uint16_t block_align;
+    uint16_t bits_per_sample;
+
+    char subchunk2_id[4];
+    uint32_t subchunk2_size;
+};
+
+union header_data {
+    char data[HEADER_SIZE];
+    wav_header header;
+};
+
+union header_data *file_header;
+
+
+FILE* read_wav_file(char* fileName) {
+    FILE* wav_ptr;
+    wav_ptr = fopen(fileName, "r");
+
+    if (!wav_ptr) {
+        fprintf(stderr, "%s can't be opened\n", fileName);
         exit(1);
     }
 
-    FILE *fp, *new_fp;
-    fp = fopen(*(argv + 1), "r");
-    new_fp = fopen(*(argv + 2), "w");
-    float change_value = atof(*(argv + 3));
+    file_header = (union header_data *) malloc(HEADER_SIZE);
+    for (int i = 0; i < HEADER_SIZE && (file_header->data[i] = getc(wav_ptr)) != EOF; i++);
 
-    if (!fp) {
-        fprintf(stderr, "%s file can't be opened\n", *(argv + 1));
-        exit(1);
-    }
-
-    if (!new_fp) {
-        fprintf(stderr, "%s file can't be opened\n", *(argv + 2));
-        exit(1);
-    }
-
-    union header_data *file_header = (union header_data *) malloc(HEADER_SIZE);
-
-    for (int i = 0; i < HEADER_SIZE && (file_header->data[i] = getc(fp)) != EOF; i++);
-
-    printf("ChunkID: %.4s\n", file_header->header.id_descr.chunk_id_s);
+    printf("ChunkID: %.4s\n", file_header->header.chunk_id);
     printf("Chunk size: %u\n", file_header->header.chunk_size);
-    printf("Format: %.4s\n", file_header->header.format_descr.format_s);
+    printf("Format: %.4s\n", file_header->header.format);
 
-    printf("Subchunk 1 ID: %s\n", file_header->header.subchunk1_descr.subchunk1_id_s);
+    printf("Subchunk 1 ID: %s\n", file_header->header.subchunk1_id);
     printf("Subchunk 1 size: %u\n", file_header->header.subchunk1_size);
-    printf("Audio format: %u\n", file_header->header.audio_format_descr.audio_format);
+    printf("Audio format: %u\n", file_header->header.audio_format);
     printf("Number of channels: %us\n", file_header->header.num_channels);
     printf("Sample rate: %u\n", file_header->header.sample_rate);
     printf("Byte rate: %u\n", file_header->header.byte_rate);
     printf("Block align: %us\n", file_header->header.block_align);
     printf("Bits per sample: %us\n", file_header->header.bits_per_sample);
 
-    printf("Subchunk 2 ID: %.4s\n", file_header->header.subchunk2_descr.subchunk2_id_s);
+    printf("Subchunk 2 ID: %.4s\n", file_header->header.subchunk2_id);
     printf("Subchunk 2 size: %u\n", file_header->header.subchunk2_size);
 
     printf("\nChanging volume of wav file\n");
 
+    return wav_ptr;
+}
 
+FILE* write_edited_data(char* fileName, FILE* dataSource, float changeValue) {
+    FILE* edited_wav;
+    edited_wav = fopen(fileName, "w");
 
-    // writing header of read file to the new file
+    if (!edited_wav) {
+        fprintf(stderr, "%s can't be opened\n", fileName);
+        exit(1);
+    }
 
     for (int i = 0; i < HEADER_SIZE; i++) {
-        putc( file_header->data[i], new_fp);
+        putc( file_header->data[i], edited_wav);
     }
 
-    // writing edited data to the new file
+    // writing edited data to new file
     uint16_t buffer;
-    while (fread(&buffer, 2, 1, fp)) {
-        buffer = buffer * (float)change_value;
-        fwrite(&buffer, 2, 1, new_fp);
+    while (fread(&buffer, 2, 1, dataSource)) {
+        buffer = buffer * (float)changeValue;
+        fwrite(&buffer, 2, 1, edited_wav);
     }
 
-    fclose(fp);
-    fclose(new_fp);
-    exit(0);
+    return edited_wav;
 }
