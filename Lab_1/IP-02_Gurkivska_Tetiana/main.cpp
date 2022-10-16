5771464dc88e52c2c5beec23289774be8ddb09f1
Tatianka-hk <t0681689150@gmail.com>
2022-10-16 17:32:40 +0200

    Lab_1

---
+++ Lab_1/IP-02_Gurkivska_Tetiana/main.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "wav.h"
+
+
+int main()
+{
+    header wav;
+    wav.read_data();
+    wav.print_data();
+    wav.change_and_safe();
+}
