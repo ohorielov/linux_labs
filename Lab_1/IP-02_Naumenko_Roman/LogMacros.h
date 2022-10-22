bf6b75ce46201d4e8579ff4e3e98268a2f5e5758
Roman Naumenko <roman@Fugaku.localdomain>
2022-10-22 17:13:22 +0300

    Removed LogMacros.h.

--- Lab_1/IP-02_Naumenko_Roman/LogMacros.h
+++
@@ -1,7 +0,0 @@
-#if DEBUG
-    #define InfoLog(info) std::cout << info << std::endl
-    #define DebugLog(info) std::cout <<  "DebugLog[" << __FILE__ << ":" << __LINE__ << "]: " << info << std::endl
-#else
-    #define InfoLog(info) 0
-    #define DebugLog(info) 0 
-#endif
