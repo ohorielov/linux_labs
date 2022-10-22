e6dde22ef5d544acf296ec8cd835489864c090de
Roman Naumenko <roman@Fugaku.localdomain>
2022-10-22 17:20:46 +0300

    Restored lost changes in main.cpp

--- Lab_1/IP-02_Naumenko_Roman/main.cpp
+++ Lab_1/IP-02_Naumenko_Roman/main.cpp
@@ -1,10 +1,24 @@
-#include "WavManager.h"
+#include <iostream>
+#include <string>
+#include "WavHandler.h"
+#include "Macroses.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    WavManager wavManager;
+    if (argc < 4)
+    {
+        InfoLog("Some arguments are not specified.");
+        InfoLog("Example: ./main inputFile.txt outputFile.txt 0.4");
+        return -1;
+    }
 
-    wavManager.IncreaseVolume("../Master Of Puppets.wav", "OutFile_VolumeUp.wav");
+    float factor = std::stof(argv[3]);
 
-    return 0;
+    if (factor > 1.0f || factor <= 0.0f)
+    {
+        DebugLog("Volume scale factor value must be in range of (0, 1].");
+        return -1;
+    }
+
+    return WavHandler{}.ScaleVolume(argv[1], argv[2], factor);
 }
