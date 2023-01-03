ee5029ca458abfd01651e88e1eea599086d7c761
ChuenkoD <chuenko04@gmail.com>
2023-01-03 14:55:44 +0200

    Lab_1: Lab1 done

---
+++ Lab_1/IP-04_Chuenko_Dmytro/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "program.h"
+
+int main()
+{
+    std::cout << "Func start";
+    Program audiob;
+    audiob.Algorithm();
+    std::cout << "Func end";
+
+    return 0;
+}
