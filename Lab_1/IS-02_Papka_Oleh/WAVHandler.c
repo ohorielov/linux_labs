5750f8075c72fbb697d8ce71dc4bd339665830e5
Oleh Papka <76498263+olehpapka@users.noreply.github.com>
2023-01-06 18:36:35 +0200

    Slight changes

--- Lab_1/IS-02_Papka_Oleh/WAVHandler.c
+++ Lab_1/IS-02_Papka_Oleh/WAVHandler.c
@@ -20,10 +20,10 @@ WAV* loadWAVFile(const char* name)
     
     wav->dataSubChunk.data = (uint8_t*)malloc(fileSize);
 
-    fread((uint8_t*)&wav->riff, 1, riffSize, file);
-    fread((uint8_t*)&wav->fmtSubChunk, 1, fmtChunkSize, file);
-    fread((uint8_t*)&wav->dataSubChunk, 1, dataChunkSize, file);
-    fread(wav->dataSubChunk.data, 1, fileSize - WAVMetadataSize, file);
+    fread((uint8_t*)&wav->riff, riffSize, 1, file);
+    fread((uint8_t*)&wav->fmtSubChunk, fmtChunkSize, 1, file);
+    fread((uint8_t*)&wav->dataSubChunk, dataChunkSize, 1, file);
+    fread(wav->dataSubChunk.data, fileSize - WAVMetadataSize, 1, file);
     
     fclose(file);
 
@@ -42,24 +42,19 @@ void handleWAV(WAV* wav, const char* name)
 
     float multiplier = 0.01;
 
-    fwrite((uint8_t*)&wav->riff, 1, riffSize, file);
-    fwrite((uint8_t*)&wav->fmtSubChunk, 1, fmtChunkSize, file);
-    fwrite((uint8_t*)&wav->dataSubChunk, 1, dataChunkSize, file);
+    fwrite((uint8_t*)&wav->riff, riffSize, 1, file);
+    fwrite((uint8_t*)&wav->fmtSubChunk, fmtChunkSize, 1, file);
+    fwrite((uint8_t*)&wav->dataSubChunk, dataChunkSize, 1, file);
     
     for (uint32_t i = 0; i < wav->dataSubChunk.subChunk2Size; ++i)
     {
         uint8_t tempVal = wav->dataSubChunk.data[i] * multiplier;
         double newVal = round(tempVal);
 
-        if (newVal >= UINT8_MAX) 
-            tempVal = UINT8_MAX;
-         else if (newVal <= 0) 
-            tempVal = 0;
-         else 
-            tempVal = (uint8_t)newVal;
-        fwrite(&tempVal, 1, sizeof(uint8_t), file);
+        tempVal = (uint8_t)newVal;
+        fwrite(&tempVal, sizeof(uint8_t), 1, file);
     }
-
+    
     fclose(file);
 }
 
