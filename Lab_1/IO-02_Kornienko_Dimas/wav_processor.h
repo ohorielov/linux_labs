24b28629e4e40cb7328ec158817d1d08408edfc0
Dimas Kornienko <33063652+beanomen4@users.noreply.github.com>
2023-01-07 00:15:14 +0200

    Revert "Lab 1: Done"
    This reverts commit 50190e139e41685c2054cbd667d78014fa3a7563.

--- Lab_1/IO-02_Kornienko_Dimas/wav_processor.h
+++
@@ -1,8 +0,0 @@
-
-
-#include "wav_header.h"
-
-void load_wav(const char*, wav_header*, int16_t**);
-void write_wav(const char*, wav_header*, int16_t*);
-void adjust_wav(wav_header*, int16_t*, float);
-void structure_wav(wav_header*);
