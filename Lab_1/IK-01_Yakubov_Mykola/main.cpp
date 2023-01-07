c87e969bb2acffd15f502a9d5d8ba6a3922ec360
Kusonori <nick2002.yakubov@gmail.com>
2023-01-07 17:06:27 +0200

    Clean commit
    Changed data types
    Deleted unnecessary includes
    Deleted garbage lines
    Made small improvements

--- Lab_1/IK-01_Yakubov_Mykola/main.cpp
+++ Lab_1/IK-01_Yakubov_Mykola/main.cpp
@@ -2,17 +2,14 @@
 #include <cstdlib>
 #include <string>
 #include <fstream>
-#include <cstdint>
 #include <cstring>
-#include <cmath>
-#include <unistd.h>
 
 typedef struct  WavHeader
 {
-    uint32_t         ChunkID;        
+    uint32_t        ChunkID;        
     uint32_t        ChunkSize;      
-    uint32_t         Format;        
-    uint32_t         Subchunk1ID;        
+    uint32_t        Format;        
+    uint32_t        Subchunk1ID;        
     uint32_t        Subchunk1Size;  
     uint16_t        AudioFormat;    
     uint16_t        NumOfChan;      
@@ -20,11 +17,11 @@ typedef struct  WavHeader
     uint32_t        bytesPerSec;    
     uint16_t        blockAlign;    
     uint16_t        bitsPerSample;  
-    uint32_t         Subchunk2ID;
+    uint32_t        Subchunk2ID;
     uint32_t        Subchunk2Size; 
 } wav_hdr;
 
-int16_t* readFileData (FILE* wavFile, wav_hdr WavHeader, uint64_t numOfSamples) {
+int16_t* readFileData (FILE* wavFile, wav_hdr WavHeader, int numOfSamples) {
     int16_t* wavData = new int16_t[numOfSamples];
     std::cout << numOfSamples << "\n";
     fread(wavData, WavHeader.bitsPerSample/8, numOfSamples, wavFile);
@@ -35,16 +32,13 @@ void writeHeader (FILE* wavFile, wav_hdr WavHeader) {
     fwrite(&WavHeader, sizeof(wav_hdr), 1, wavFile);
 }
 
-void writeFileData (FILE* wavFile, int16_t* fileData, wav_hdr WavHeader, uint64_t numOfSamples) {
-    int i = 0;
+void writeFileData (FILE* wavFile, int16_t* fileData, wav_hdr WavHeader, int numOfSamples) {
     fwrite(fileData, WavHeader.bitsPerSample/8, numOfSamples, wavFile);
 }
 
-void changeVolume(int16_t* fileData, wav_hdr WavHeader, double k, uint64_t numOfSamples){
-    int i = 0;
-    while(i<numOfSamples){
+void changeVolume(int16_t* fileData, wav_hdr WavHeader, float k, int numOfSamples){
+    for(int i = 0; i < numOfSamples; i++){
         fileData[i] = (int16_t)(fileData[i]*k);
-        i++;
     }
 }
 
@@ -62,7 +56,7 @@ int main(){
     }
     fread(&WavHeader,headerSize, 1, wavFile);
     int16_t *fileData;
-    uint64_t numOfSamples = WavHeader.ChunkSize/(WavHeader.bitsPerSample/8);
+    int numOfSamples = WavHeader.ChunkSize/(WavHeader.bitsPerSample/8);
     fileData = readFileData(wavFile, WavHeader, numOfSamples);
     changeVolume(fileData, WavHeader, 2, numOfSamples);
     fclose(wavFile);
