819f5dd1e2537bdd6495d9ec4519c74d32a9ecf5
ValeriiBabich <valera_babich@ukr.net>
2023-01-07 12:22:38 +0200

    Lab_1: delete bin files

--- Lab_1/IO-02_Babich_Valerii/main.c
+++ Lab_1/IO-02_Babich_Valerii/main.c
@@ -2,31 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
-
 int main(int argc, char *argv[])
 {
-    printf("start");
-
-    FILE *input = fopen("Master\ Of\ Puppets.wav", "r");
+    FILE *input = fopen("input.wav", "r");
     FILE *output = fopen("output.wav", "w");
-
     float change = 2.0;
-
-    uint8_t headerbuffer[44];
-    fread(headerbuffer, sizeof(uint8_t), 44, input);
-    fwrite(headerbuffer, sizeof(uint8_t), 44, output);
-
+    uint8_t headerbuffer[HEADER_SIZE];
+    fread(headerbuffer, sizeof(uint8_t), HEADER_SIZE, input);
+    fwrite(headerbuffer, sizeof(uint8_t), HEADER_SIZE, output);
     uint16_t buffer;
     while(fread(&buffer, sizeof(uint16_t), 1, input)){
         buffer *= change;
         fwrite(&buffer, sizeof(uint16_t), 1, output);
     }
-
     fclose(input);
     fclose(output);
-
     return 0;
 }
