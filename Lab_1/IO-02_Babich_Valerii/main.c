028bdd377f5c227f9bc0853ff48995711b1c7b9c
ValeriiBabich <valera_babich@ukr.net>
2023-01-09 11:27:14 +0200

    Lab_1: final remarks

--- Lab_1/IO-02_Babich_Valerii/main.c
+++ Lab_1/IO-02_Babich_Valerii/main.c
@@ -1,17 +1,14 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-// Number of bytes in .wav header
-const int HEADER_SIZE = 44;
 int main(int argc, char *argv[])
 {
     FILE *input = fopen("input.wav", "r");
     FILE *output = fopen("output.wav", "w");
     float change = 2.0;
-    uint8_t headerbuffer[HEADER_SIZE];
-    fread(headerbuffer, sizeof(uint8_t), HEADER_SIZE, input);
-    fwrite(headerbuffer, sizeof(uint8_t), HEADER_SIZE, output);
+    uint8_t headerbuffer[44];
+    fread(headerbuffer, sizeof(uint8_t), 44, input);
+    fwrite(headerbuffer, sizeof(uint8_t), 44, output);
     uint16_t buffer;
     while(fread(&buffer, sizeof(uint16_t), 1, input)){
         buffer *= change;
