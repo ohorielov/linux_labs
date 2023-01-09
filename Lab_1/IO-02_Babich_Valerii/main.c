5aa596d3850a2ac2681a6391cca196117ecb8f3b
ValeriiBabich <valera_babich@ukr.net>
2023-01-09 11:34:05 +0200

    Lab_1: final final remarks

--- Lab_1/IO-02_Babich_Valerii/main.c
+++ Lab_1/IO-02_Babich_Valerii/main.c
@@ -5,13 +5,12 @@ int main(int argc, char *argv[])
 {
     FILE *input = fopen("input.wav", "r");
     FILE *output = fopen("output.wav", "w");
-    float change = 2.0;
     uint8_t headerbuffer[44];
     fread(headerbuffer, sizeof(uint8_t), 44, input);
     fwrite(headerbuffer, sizeof(uint8_t), 44, output);
     uint16_t buffer;
     while(fread(&buffer, sizeof(uint16_t), 1, input)){
-        buffer *= change;
+        buffer *= 2.0;
         fwrite(&buffer, sizeof(uint16_t), 1, output);
     }
     fclose(input);
