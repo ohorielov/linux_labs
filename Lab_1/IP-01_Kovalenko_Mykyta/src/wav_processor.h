085cd55a606bef5bbd7b47585148aec160a6edef
Dimas Kornienko <33063652+beanomen4@users.noreply.github.com>
2023-01-14 00:22:29 +0200

    Lab_2: Huffman algoritm
    My lab 2 - done

--- Lab_1/IP-01_Kovalenko_Mykyta/src/wav_processor.h
+++
@@ -1,6 +0,0 @@
-#include "wav_header.h"
-
-void load_wav(const char*, wav_header*, int16_t**);
-void write_wav(const char*, wav_header*, int16_t*);
-void adjust_wav(wav_header*, int16_t*, float);
-void dump_wav(wav_header*);
