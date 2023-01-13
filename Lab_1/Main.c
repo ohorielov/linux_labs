21dda655fdbe599958a6df8c1db5d91327ce0bb8
kkatkot <81512097+kkatkot@users.noreply.github.com>
2023-01-13 19:33:56 +0200

    Update Main.c

--- Lab_1/Main.c
+++ Lab_1/Main.c
@@ -4,10 +4,10 @@
 int main (int argc, char *argv[]) {
     struct WAV_HEADER header;
     short *audio_data;
-    read_wav_file("input.wav", &header, &audio_data);
+    read_wav_file(D:\"Master Of Puppets.wav", &header, &audio_data);
     int data_size = header.data_size;
     decrease_volune(audio_data, data_size, 0);
-    write_wav_file("input. wav", header, audio_data, data_size);
+    write_wav_file("Master Of Puppets.wav", header, audio_data, data_size);
     free(audio_data);
     return 0;
     }
