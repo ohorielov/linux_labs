d50966f8e7d9ec32c64b2eacca795856f3546c54
kkatkot <81512097+kkatkot@users.noreply.github.com>
2023-01-13 18:45:40 +0200

    Update Main.c

--- Lab_1/Main.c
+++ Lab_1/Main.c
@@ -1,11 +1,15 @@
-#include "wave.h"
 
-int main(){
-    struct WavHeader wav_header r = {}
-    FILE *file_input = 1 fopen(input, "rb");
-    read_header (file_input, &wav_header);
-    print_header_data(&wav_header);
-    change_volume(&wav_header, VOLUME);
+#include <stdlib.h>
+#include "WAV_HEADER.h"
+int main (int argc, char *argv[]) {
+    struct WAV_HEADER header;
+    short *audio_data;
+    read_wav_file( filename: "input.wav", &header, &audio_data);
+    int data_size = header. data_size;
+    decrease_volune (audio_data, data_size,
+    factor: 0);
+    write_wav_file( filename: "input. wav", header, audio_data, data_size);
+    free( ptr: audio_data);
     return 0;
+    }
 
-}
