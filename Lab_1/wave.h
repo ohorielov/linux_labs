315ff86bb66a7bf6768ec835e68e80846807be75
kkatkot <81512097+kkatkot@users.noreply.github.com>
2023-01-13 18:52:09 +0200

    Update wave.h

--- Lab_1/wave.h
+++ Lab_1/wave.h
@@ -1,30 +1,19 @@
+struct WAV_HEADER {
+char riff[4];
+int riff_size;
+char wave[4];
+char fmt[4];
+int fmt_size;
+short audio_format;
+short nun_channels;
+int sample_rate;
+int byte_rate;
+short block_align;
+short bits_per_sample;
+char data[4]
+int data_size;
+};
 
-struct wav_riff {
-  char chunk_id[4];
-  union int_data chunk_size;
-  char format [4];
-};
-struct wav_fmt {
-  char subchunkl_id[4];
-  union int_data subchunk1_size;
-  union short_data audio_format;
-  union short_data num_channels;
-  union int_data sample_rate;
-  union int_data byte_rate;
-  union short_data block_align;
-  union short_data bits_per_sample;
-};
-struct wav_data {
-  char subchunk2_id[4];
-  union int_data subchunk2_size;
-  union short_data *data;
-};
-union wav{
-    byte data[HEADER_SIZE];
-    struct wav_riff riff;
-    struct wav_fmt fmt;
-    struct wav_data data;
-}
-void read_header(FILE *fp, union header_data *file_bytes, char *file_name);
-void print_header_data(union header_data *file_bytes);
-void change_volume(wav *wh, float scale);
+int read_wav_file(const char *filename, struct WAV_HEADER *header, short **audio_data);
+void decrease_volume(short *audio_data, int data_size, float factor);
+int write_wav_file(const char *filename, struct WAV_HEADER header, short *audio_data, int data_size);
